38bd69310c4ac2c130df3b42d0015fcba8695ec8
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-06-14 01:47:29 +0200

    Add request API quotes

---
+++ script.js
@@ -0,0 +1,24 @@
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // error handling
+    console.log(error);
+  }
+}
+
+// on load
+getQuotes();
