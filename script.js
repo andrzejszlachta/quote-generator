9c47cc663fbb987682483f384bb3efbbfdeac6d2
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-06-14 02:46:10 +0200

    Add loading spinner

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const quoteContainer = document.getElementById('quote-container');
+const loader = document.getElementById('loader');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
@@ -6,8 +7,21 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 
+// Show loading spinner
+function startLoading() {
+  quoteContainer.hidden = true;
+  loader.hidden = false;
+}
+
+// Hide loading spinner
+function stopLoading() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // Show new quote
 function newQuote() {
+    startLoading()
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank
@@ -22,11 +36,14 @@ function newQuote() {
     } else {
       quoteText.classList.remove('long-quote')
     }
+    // set quote & hide loading spinner
     quoteText.innerText = quote.text
+    stopLoading()
 }
 
 // Get quotes from API
 async function getQuotes() {
+  startLoading()
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
